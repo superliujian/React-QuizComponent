6d306d3e8d85f458ce12eb491130c23691952644
HOME-WINDOWS7\Jian <liu_jian_china@qq.com>
2018-06-07 16:51:30 +0800

    module1

---
+++ src/Quiz.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react';
+let quizData=require('./quiz_data.json');
+class Quiz extends Component{
+
+    constructor(props){
+        super(props);
+        this.setState(
+            {quiz_position:1}
+        );
+    }
+
+    return (){
+        return(
+            <div>
+                <div className='QuizQUestion'>
+                    {quizData.quiz_questions[0].instruction_text}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Quiz;
